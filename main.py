b71e8c63e66bdcabccd3aba7f67c580bb4060754
Josh Alexakos <josiah.alexakos@gmail.com>
2025-05-30 08:22:45 -0700

    Add player class and add it to game

--- main.py
+++ main.py
@@ -3,18 +3,21 @@
 # throughout this file
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_SPAWN_RATE, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_MAX_RADIUS
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
